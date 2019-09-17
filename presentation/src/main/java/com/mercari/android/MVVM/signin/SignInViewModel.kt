a4487c111f5ac279afedc94e53f733b2bd7a3984
mahdi <mahdi.tajik@gmail.com>
2019-09-17 10:53:03 +0430

    Add some services.

---
+++ presentation/src/main/java/com/mercari/android/MVVM/signin/SignInViewModel.kt
@@ -0,0 +1,7 @@
+package com.mercari.android.MVVM.signin
+
+import androidx.lifecycle.ViewModel
+
+class SignInViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ presentation/src/main/java/com/mercari/android/MVVM/signup/SignUpViewModel.kt
@@ -0,0 +1,7 @@
+package com.mercari.android.MVVM.signup
+
+import androidx.lifecycle.ViewModel
+
+class SignUpViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
